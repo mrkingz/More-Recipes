8f121c4b9bd6cd9b7ec041556f60c2a8b989082b
Kingsley Frank-Demesi <kingsleyfrankdemesi@gmail.com>
2017-10-27 10:26:01 -0700

    update develop

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -4,5 +4,6 @@ import RecipeController from './../controllers/recipeController';
 const recipeRouter = express.Router();
 
 recipeRouter.route('/api/v1/recipes')
-.post(RecipeController.createRecipe())
-.get(RecipeController.getRecipes());
+  .post(RecipeController.createRecipe())
+  .get(RecipeController.getRecipes());
+export default recipeRouter;
