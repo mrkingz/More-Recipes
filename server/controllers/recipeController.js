ea5b6c5822eb3cbf58ce9215f8db9e82a9a5caed
Kingsley Frank-Demesi <kingsleyfrankdemesi@gmail.com>
2017-10-27 11:56:26 -0700

    add update recipe

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -3,25 +3,24 @@ const recipes = [];
 /**
  * @class RecipeController
  */
-class RecipeController
-{
+class RecipeController {
   /**
-   * @description Creates a new Recipe 
+   * @description Creates a new Recipe
    * @static
    * @memberof RecipeController
    * @return {function} A middleware function that handles the POST request
    */
-    static createRecipe()
-    {
-        return (req, res) => {
-            recipes.push(req.body);
-            res.status(201).json({
-                success: true,
-                message: 'Recipe successfully created',
-                data: req.body
-            })
-        }
-    }
+  static createRecipe() {
+    return (req, res) => {
+      //req.body.recipeId = recipes.length + 1;
+      recipes.push(req.body);
+      res.status(201).json({
+        success: true,
+        message: 'Recipe successfully created',
+        data: req.body
+      });
+    };
+  }
 
   /**
    * @description Gets all recipes
@@ -29,16 +28,48 @@ class RecipeController
    * @memberof RecipeController
    * @returns {function} A middleware funtion that handles the GET request
    */
-    static getRecipes()
-    {
-        return (req, res) => {
-            if(!req.query.sort) {
-                res.status(200).send(recipes);
-            } else {
-                const results = recipes.sort((a, b) => (b.upvote - a.upvote));
-                res.status(200).send(results);   
-            }
+  static getRecipes() {
+    return (req, res) => {
+      if (!req.query.sort) {
+        res.status(200).send(recipes);
+      } else {
+        const results = recipes.sort((a, b) => (b.upvote - a.upvote));
+        res.status(200).send(results);
+      }
+    };
+  }
+
+  /**
+   * @description Updates a recipe
+   * @static
+   * @memberof RecipeController
+   * @returns {Function} A middleware funtion that handles the PUT request
+   */
+  static updateRecipe() {
+    return (req, res) => {
+      let notFound = true;
+      for (let i = 0; i < recipes.length; i += 1) {
+        if (recipes[i].recipeId === req.params.recipeId) {
+          recipes[i].recipeTitle = req.body.recipeTitle || recipes[i].recipeTitle;
+          recipes[i].ingredients = req.body.ingredients || recipes[i].ingredients;
+          recipes[i].instructions = req.body.instructions || recipes[i].instructions;
+          recipes[i].upVote = req.body.upVote || recipes[i].upVote;
+          recipes[i].downVote = req.body.downVote || recipes[i].downVote;
+          notFound = false;
+          res.status(200).send({
+            success: true,
+            message: 'recipe successfully edited.',
+            data: recipes
+          });
         }
-    }
+      }
+      if (notFound) {
+        res.status(404).json({
+          success: false,
+          message: 'recipe not found'
+        });
+      }
+    };
+  }
 }
 export default RecipeController;
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -2,8 +2,13 @@ import express from 'express';
 import RecipeController from './../controllers/recipeController';
 
 const recipeRouter = express.Router();
-
 recipeRouter.route('/api/v1/recipes')
   .post(RecipeController.createRecipe())
   .get(RecipeController.getRecipes());
+
+
+recipeRouter.put(
+  '/api/v1/recipes/:recipeId',
+  RecipeController.updateRecipe()
+);
 export default recipeRouter;
